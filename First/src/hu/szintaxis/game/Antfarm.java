018eb23087d81f8b31f9c3d1598fa623606479a1
Dóczi Róbert <drobi@tritel.hu>
2013-04-24 14:57:57 +0200

    Példa engine használatra

---
+++ First/src/hu/szintaxis/game/Antfarm.java
@@ -0,0 +1,21 @@
+package hu.szintaxis.game;
+
+import java.awt.Graphics2D;
+
+import hu.szintaxis.game.engine.Game;
+
+public class Antfarm extends Game{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
