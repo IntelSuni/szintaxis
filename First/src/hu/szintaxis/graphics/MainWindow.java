c559f5df64c5366ca107e2676cbfd65a7f0c90c4
IntelSuni <sandorrmartin@gmail.com>
2013-04-26 13:02:38 +0200

    Grafikus felület próba.

---
+++ First/src/hu/szintaxis/graphics/MainWindow.java
@@ -0,0 +1,56 @@
+package hu.szintaxis.graphics;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JSplitPane;
+import java.awt.Canvas;
+
+public class MainWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane, BorderLayout.NORTH);
+		
+		JSplitPane splitPane_1 = new JSplitPane();
+		splitPane.setRightComponent(splitPane_1);
+		
+		JSplitPane splitPane_2 = new JSplitPane();
+		splitPane_1.setRightComponent(splitPane_2);
+		
+		Canvas canvas = new Canvas();
+		contentPane.add(canvas, BorderLayout.CENTER);
+	}
+
+}
